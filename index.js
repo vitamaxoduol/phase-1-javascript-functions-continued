f5cdccbd4e0e2938812bdac8a8f08529aa76b6b3
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-16 01:57:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    
+    return `This Saturday, I want to ${activity}!`
+
+}
+console.log(saturdayFun('bathe my dog'));
+
+function mondayWork (activity) {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork('work from home'));
